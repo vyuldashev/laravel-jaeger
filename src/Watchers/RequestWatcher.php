76a252101fc93b2a3b88d282d41116401b512896
Vladimir Yuldashev <misterio92@gmail.com>
2019-07-26 21:10:26 +0300

    wip

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -18,15 +18,13 @@ class RequestWatcher
     public function register(): void
     {
         Event::listen(RequestHandled::class, function (RequestHandled $event) {
-//            logger('RequestHandled');
-
             $rootSpan = $this->jaeger->getRootSpan();
             $rootSpan->overwriteOperationName(optional($event->request->route())->uri() ?? $event->request->getPathInfo());
             $rootSpan->setTag('http.scheme', $event->request->getScheme());
             $rootSpan->setTag('http.host', $event->request->getHost());
             $rootSpan->setTag('http.path', str_replace($event->request->root(), '', $event->request->fullUrl()) ?: '/');
             $rootSpan->setTag('http.method', $event->request->method());
-            $rootSpan->setTag('http.status_code', (string)$event->response->status());
+            $rootSpan->setTag('http.status_code', (string)$event->response->getStatusCode());
             $rootSpan->setTag('http.error', $event->response->isSuccessful() ? 'false' : 'true');
             $rootSpan->setTag('laravel.controller_action', optional($event->request->route())->getActionName());
 
